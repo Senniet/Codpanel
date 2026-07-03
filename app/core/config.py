12768e5246a19e00eda0e150ec70607d21ee05c4
root <root@Spotweb.lg1leb1q1b1u3nihs5dxmqzdpg.fx.internal.cloudapp.net>
2026-07-03 16:19:22 +0000

    Initial project structure

---
+++ app/core/config.py
@@ -0,0 +1,13 @@
+SERVER_NAME = "COD1 Server"
+
+SERVER_PATH = "/data/myserver"
+
+SERVER_BINARY = "/data/myserver/cod_lnxded"
+
+SERVER_CONFIG = "/data/myserver/myserver.cfg"
+
+SERVER_USER = "codserver"
+
+SERVER_IP = "172.16.0.4"
+
+SERVER_PORT = 28960
